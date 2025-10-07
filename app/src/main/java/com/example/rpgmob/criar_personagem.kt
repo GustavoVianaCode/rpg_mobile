8d880adb89855293ce86dc67ce1c5623a3edf5cc
GustavoVianaCode <vianagust4@gmail.com>
2025-10-06 22:41:14 -0300

    1.1
    Menu - ?
    Mudança de tela - ok
    ImageView - ok
    Entrada de dados - ?

--- app/src/main/java/com/example/rpgmob/criar_personagem.kt
+++ app/src/main/java/com/example/rpgmob/criar_personagem.kt
@@ -1,20 +1,14 @@
 package com.example.rpgmob
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class criar_personagem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val criarbtn = findViewById<Button>(R.id.btnCreate)
-        criarbtn.setOnClickListener {
-            startActivity(Intent(this, criar_personagem::class.java))
-        }
+        setContentView(R.layout.activity_criar_personagem)
     }
 }
